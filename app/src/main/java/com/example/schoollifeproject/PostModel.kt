e2f21bb93dc73e5a8dbd25a8b512ede9326493af
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-03-13 15:28:56 +0900

    레트로핏 1.0.0

---
+++ app/src/main/java/com/example/schoollifeproject/PostModel.kt
@@ -0,0 +1,17 @@
+package com.example.schoollifeproject
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class PostModel(
+    @Expose
+    @SerializedName("userID")
+    var userID: String? = null,
+    @Expose
+    @SerializedName("userPassword")
+    var userPassword: String? = null,
+    @Expose
+    @SerializedName("userName")
+    var userName: String? = null
+
+)
