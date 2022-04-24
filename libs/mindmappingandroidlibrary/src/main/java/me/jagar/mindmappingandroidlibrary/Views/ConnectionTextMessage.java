acda9c0baae76b83a016251019c6cab2491d33cc
Laeti <creator98@naver.com>
2022-04-24 18:36:36 +0900

    220424 PDH Delete File

--- libs/mindmappingandroidlibrary/src/main/java/me/jagar/mindmappingandroidlibrary/Views/ConnectionTextMessage.java
+++
@@ -1,9 +0,0 @@
-package me.jagar.mindmappingandroidlibrary.Views;
-
-import android.content.Context;
-
-public class ConnectionTextMessage extends androidx.appcompat.widget.AppCompatTextView {
-    public ConnectionTextMessage(Context context) {
-        super(context);
-    }
-}
