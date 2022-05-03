c23e56dc3cc344662baf52fafe2dd795a1a0b088
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-05-03 15:05:45 +0900

    0503 LJY

--- app/src/main/java/com/example/schoollifeproject/MenuActivity.kt
+++ app/src/main/java/com/example/schoollifeproject/MenuActivity.kt
@@ -12,44 +12,51 @@ import retrofit2.Response
 
 
 class MenuActivity : AppCompatActivity() {
+    private var flag = 0
+    private var countKey: Int = 0
+    private lateinit var id: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val api = APIS_login.create()
-        var countKey: Int = 0
-        val id = intent.getStringExtra("ID").toString()
 
-
-        val btnMap = binding.btnMap
+        id = intent.getStringExtra("ID").toString()
+        val btnMain = binding.btnMain
         val btnNotice = binding.btnNotice
+        val btnMap = binding.btnMap
+        val btnSetting = binding.btnSetting
         val tvNick = binding.tvNick
 
-        tvNick.setText(intent.getStringExtra("name")+"님 환영합니다")
+        tvNick.setText(intent.getStringExtra("name") + "님 환영합니다")
 
-        api.notice_key_search(1 )
+        api.notice_key_search(1)
             .enqueue(object : Callback<PostModel> {
                 override fun onResponse(
                     call: Call<PostModel>,
                     response: Response<PostModel>
                 ) {
 
-                    if(!response.body().toString().isEmpty()) {
+                    if (!response.body().toString().isEmpty()) {
                         countKey = response.body()?.countKey!!
                     }
                     Log.d("키갯수", countKey.toString())
                 }
+
                 override fun onFailure(p0: Call<PostModel>, t: Throwable) {
                     Log.d("failedSearchKey", t.message.toString())
                 }
             })
 
 
+        btnMain.setOnClickListener {
+            flag = 0
+            removeFragment()
+        }
         btnNotice.setOnClickListener {
-            val intent = Intent(this, ListActivity::class.java)
-            intent.putExtra("ID", id)
-            intent.putExtra("key", countKey)
-            startActivity(intent)
+            flag = 1
+            switchFragment()
+
         }
 
         btnMap.setOnClickListener {
@@ -58,6 +65,45 @@ class MenuActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun switchFragment() {
+        var listFragment = ListFragment()
+        var bundle = Bundle()
+        bundle.putString("ID", id)
+        bundle.putInt("countKey", countKey)
+        listFragment.arguments = bundle
+
+
+
+
+
+        val transaction = supportFragmentManager.beginTransaction()
+        when (flag) {
+            1 -> {
+
+                transaction.replace(R.id.frameLayout, listFragment)
+                transaction.commit()
+            }
+           /* 2 -> {
+                transaction.replace(R.id.frameLayout, FragmentB())
+            }
+            3 -> {
+                transaction.replace(R.id.frameLayout, FragmentA())
+            }
+            4 -> {
+
+            }*/
+        }
+
+
+    }
+
+    private fun removeFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        val frameLayout = supportFragmentManager.findFragmentById(R.id.frameLayout)
+        transaction.remove(frameLayout!!)
+        transaction.commit()
     }
 
 }
