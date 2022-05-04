373cdb0baa48f82e220900a9b342c600cbaf2aea
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-05-04 14:24:20 +0900

    0504 2:20

--- app/src/main/java/com/example/schoollifeproject/MenuActivity.kt
+++ app/src/main/java/com/example/schoollifeproject/MenuActivity.kt
@@ -4,30 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.schoollifeproject.adapter.AnnoListAdapter
+import com.example.schoollifeproject.adapter.SugListAdapter
 import com.example.schoollifeproject.databinding.ActivityMenuBinding
 import com.example.schoollifeproject.fragment.ListFragment
 import com.example.schoollifeproject.fragment.MindMapFragment
-import com.example.schoollifeproject.model.APIS_login
-import com.example.schoollifeproject.model.AnnoContacts
-import com.example.schoollifeproject.model.PostModel
+import com.example.schoollifeproject.model.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
 class MenuActivity : AppCompatActivity() {
-    private val annoContactslist : List<AnnoContacts> = listOf(
+    private val annoContactslist: List<AnnoContacts> = listOf(
         AnnoContacts("공지사항1"),
         AnnoContacts("공지사항2"),
         AnnoContacts("공지사항3")
     )
-/*
-    private var annoContactsList: MutableList<AnnoContacts> = mutableListOf()
-    private var contactsList: MutableList<Contacts> = mutableListOf()
-    private var contactsList: MutableList<Contacts> = mutableListOf()
-    private var contactsList: MutableList<Contacts> = mutableListOf()
-*/
-    private val adapter = AnnoListAdapter(annoContactslist)
+    private val sugContactslist: List<SugContacts> = listOf(
+        SugContacts("1번글", 3),
+        SugContacts("2번글", 2),
+        SugContacts("3번글", 1)
+    )
+
+    /*
+        private var annoContactsList: MutableList<AnnoContacts> = mutableListOf()
+        private var contactsList: MutableList<Contacts> = mutableListOf()
+        private var contactsList: MutableList<Contacts> = mutableListOf()
+        private var contactsList: MutableList<Contacts> = mutableListOf()
+    */
+    private val annoAdapter = AnnoListAdapter(annoContactslist)
+    private val sugAdapter = SugListAdapter(sugContactslist)
 
     private var countKey: Int = 0
     private lateinit var userID: String
@@ -38,13 +44,10 @@ class MenuActivity : AppCompatActivity() {
         setContentView(binding.root)
         val api = APIS_login.create()
 
-        binding.annoRecycler.adapter = adapter
-
+        binding.annoRecycler.adapter = annoAdapter
+        binding.sugRecycvler.adapter = sugAdapter
 
         userID = intent.getStringExtra("ID").toString()
-        val tvNick = binding.tvNick
-
-        tvNick.setText(intent.getStringExtra("name") + "님 환영합니다")
 
         api.notice_key_search(1)
             .enqueue(object : Callback<PostModel> {
@@ -54,7 +57,7 @@ class MenuActivity : AppCompatActivity() {
                 ) {
 
                     if (!response.body().toString().isEmpty()) {
-                        //countKey = response.body()?.countKey!!
+                        countKey = response.body()?.countKey!!
                     }
                     Log.d("키갯수", countKey.toString())
                 }
@@ -73,17 +76,24 @@ class MenuActivity : AppCompatActivity() {
 
             setOnItemSelectedListener { item ->
                 val transaction = supportFragmentManager.beginTransaction()
+                val frameLayout = supportFragmentManager.findFragmentById(R.id.frameLayout)
                 when (item.itemId) {
                     R.id.mainMenu1 -> {
-                        bundle.putInt("countKey", countKey)
-                        listFragment.arguments = bundle
-                        transaction.replace(R.id.frameLayout, listFragment)
-                            .commitAllowingStateLoss()
+                        if (frameLayout != null) {
+                            removeFragment()
+                        }
                         true
                     }
                     R.id.mainMenu2 -> {
                         mindMapFragment.arguments = bundle
                         transaction.replace(R.id.frameLayout, mindMapFragment)
+                            .commitAllowingStateLoss()
+                        true
+                    }
+                    R.id.mainMenu3 -> {
+                        bundle.putInt("countKey", countKey)
+                        listFragment.arguments = bundle
+                        transaction.replace(R.id.frameLayout, listFragment)
                             .commitAllowingStateLoss()
                         true
                     }
--- app/src/main/java/com/example/schoollifeproject/noticeActivity.kt
+++ app/src/main/java/com/example/schoollifeproject/noticeActivity.kt
@@ -3,35 +3,51 @@ package com.example.schoollifeproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.schoollifeproject.adapter.NoteReadListAdapter
 import com.example.schoollifeproject.databinding.ActivityNoticeBinding
 import com.example.schoollifeproject.model.APIS_login
+import com.example.schoollifeproject.model.Contacts
+import com.example.schoollifeproject.model.NoteReadContacts
 import com.example.schoollifeproject.model.PostModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class noticeActivity : AppCompatActivity() {
+    private var readContactsList: MutableList<NoteReadContacts> = mutableListOf()
+    private val readAdapter = NoteReadListAdapter(readContactsList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityNoticeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val api = APIS_login.create()
+        binding.noteReadRecyclerView.adapter = readAdapter
+
         val key = intent.getIntExtra("key", 0)
         val title = intent.getStringExtra("title")
+
         //해당 글의 키를 가져와 표시
         api.notice_open(
             key
         ).enqueue(object : Callback<PostModel> {
             override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
-                binding.titleView.setText(title)
-                binding.contectView.setText(response.body()?.noticeContents)
                 //작성되어있던 댓글 불러오기
                 //작성자 표시
                 //작성일 표시
+                val contacts =
+                    NoteReadContacts(
+                        title,
+                        response.body()?.noticeName,
+                        response.body()?.noticeContents
+                    )
+                readContactsList.add(contacts)
+                readAdapter.notifyDataSetChanged()
             }
 
             override fun onFailure(call: Call<PostModel>, t: Throwable) {
-                Log.d("onFailure",t.message.toString())
+                Log.d("onFailure", t.message.toString())
             }
         })
         //버튼 기능으로 댓글 작성 및 저장 최신화
